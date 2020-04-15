102065aaf284275924a8fddca4fc30e72511bf12
Sebastien Vezina <info@sebastienvezina.com>
2020-04-15 02:55:36 -0400

    edit config

--- config.py
+++ config.py
@@ -2,13 +2,11 @@ from os import environ
 import os
 
 class Config:
-    """Set Flask configuration vars from .env file."""
-
     # General
-    FLASK_DEBUG = environ.get('FLASK_DEBUG')
+    FLASK_DEBUG = False)
 
     # Database
     APP_BASEDIR = os.path.dirname(__file__)
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(APP_BASEDIR, 'db', 'app.db') #environ.get('SQLALCHEMY_DATABASE_URI')
-    SQLALCHEMY_TRACK_MODIFICATIONS = environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
-    NEWSAPI_KEY = environ.get('NEWAPI_KEY')
+    SQLALCHEMY_DATABASE_URI='sqlite:///../db/app.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+    NEWSAPI_KEY='e3591106e13347bc92a3d1ab29a5c341'
